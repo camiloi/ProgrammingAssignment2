224651abef80b179ec899e2a8790878e7b8fa68e
camiloi <camilo.ibarra.escobar@hotmail.com>
2015-07-26 15:18:59 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+ 
+##finding the set matrix or cofactors, and solve the matrix to make a list with this elements
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+set<-function(y){
+x<<-y
+m<<-NULL
 }
-
-
-## Write a short comment describing this function
+get<-function()x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
+}
+## if its the first time the function will solve the matrix, but the next time the answer is taked from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+m<-x$getmatrix()
+if(!is.null(m)){
+message("cache solved")
+return(m)
+}
+matrix<-x$get()
+m<-solve(matrix, ...)
+x$setmatrix(m)
+m
 }
